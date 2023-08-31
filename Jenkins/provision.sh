a188258ff4c7f943cc69bea7410eb6362712c9a3
lozaexequiel <lozaexequiel@outlook.com>
2023-08-31 11:09:51 -0300

    comment the init password

--- Jenkins/provision.sh
+++ Jenkins/provision.sh
@@ -9,7 +9,8 @@ install_jenkins ()
 docker pull ${DOCKER_IMAGE}
 docker network create ${DOCKER_NETWORK}
 docker run -d --name jenkins --restart unless-stopped -p ${APPLICATION_HOST}:${APPLICATION_PORT} -p ${JENKINS_AGENT_HOST}:${JENKINS_AGENT_PORT} --network ${DOCKER_NETWORK} -v /var/run/docker.sock:/var/run/docker.sock -v ${APP_HOST_VOLUME}:${APP_VOLUME} ${DOCKER_IMAGE} 
-docker exec -it jenkins bash -c "cat /var/jenkins_home/secrets/initialAdminPassword" > /vagrant_data/.env/initialPasswordJenkins.env
+#sleep 15
+#docker exec -it jenkins bash -c "cat /var/jenkins_home/secrets/initialAdminPassword" > /vagrant_data/.env/initialPasswordJenkins.env
 }
 
 variables
--- Vault/provision.sh
+++ Vault/provision.sh
@@ -12,7 +12,7 @@ sudo apt-get update
 sudo apt-get install -y gnupg2 curl software-properties-common
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install -y vault
+apt-get update && sudo apt-get install -y vault
 sudo systemctl stop vault
 cat <<EOF > /etc/vault.d/vault.hcl
     storage "raft" {
@@ -46,9 +46,9 @@ configure_vault ()
 start_vault ()
 {   sleep 1
     echo "Unsealing Vault with the Vault address ${VAULT_ADDR}"
-    vault operator unseal ${VAULT_UNSEAL_KEY}    
+    vault operator unseal ${VAULT_UNSEAL_KEY}
     vault login ${VAULT_TOKEN}
-    vault auth enable ${APPROLE_AUTH_PATH}    
+    vault auth enable ${APPROLE_AUTH_PATH}
     vault secrets enable -path=${VAULT_PATH} -version=2 kv
 }
 
@@ -63,10 +63,10 @@ EOF
 }
 
 create_approle ()
-{ 
+{
   vault write auth/${APPROLE_AUTH_PATH}/role/${APPROLE_ROLE_NAME} token_num_uses=${TOKEN_NUM_USES} secret_id_num_uses=${SECRET_ID_NUM_USES} policies=${APPROLE_POLICY_NAME}
-  vault write -f auth/${APPROLE_AUTH_PATH}/role/${APPROLE_ROLE_NAME}/secret-id > ${VAULT_SECRET_ID_FILE}  
-  vault read auth/${APPROLE_AUTH_PATH}/role/${APPROLE_ROLE_NAME}/role-id > ${VAULT_ROLE_ID_FILE}   
+  vault write -f auth/${APPROLE_AUTH_PATH}/role/${APPROLE_ROLE_NAME}/secret-id > ${VAULT_SECRET_ID_FILE}
+  vault read auth/${APPROLE_AUTH_PATH}/role/${APPROLE_ROLE_NAME}/role-id > ${VAULT_ROLE_ID_FILE}
 }
 
 create_app_secrets ()
