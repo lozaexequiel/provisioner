268f3b62f5684c30749e24cd8486f5428641b0d2
lozaexequiel <lozaexequiel@outlook.com>
2023-12-11 18:08:26 -0300

    adds docker compose install to the script

--- Docker/provision.sh
+++ Docker/provision.sh
@@ -17,8 +17,11 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-sudo systemctl status docker | grep Active
 sudo usermod -G docker ${USER}
+sudo systemctl enable docker
+sudo apt install -y docker-compose
+sudo systemctl restart docker
+sudo systemctl status docker | grep Active
 }
 
 variables
