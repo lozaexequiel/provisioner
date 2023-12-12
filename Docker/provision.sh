82ae03e6cb23e94b244f322ff8e50d692158f0bf
lozaexequiel <lozaexequiel@outlook.com>
2023-12-12 11:40:51 -0300

    removes sudo from scripts

--- Docker/provision.sh
+++ Docker/provision.sh
@@ -9,19 +9,19 @@ variables ()
 docker_provision ()
 {
 # Add Docker's official GPG key:
-sudo apt update
-sudo apt install ca-certificates curl gnupg -y
-sudo install -m 0755 -d /etc/apt/keyrings
+apt update
+apt install ca-certificates curl gnupg -y
+install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-sudo usermod -G docker ${USER}
-sudo systemctl enable docker
-sudo apt install -y docker-compose
-sudo systemctl restart docker
-sudo systemctl status docker | grep Active
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+usermod -G docker ${USER}
+systemctl enable docker
+apt install -y docker-compose
+systemctl restart docker
+systemctl status docker | grep Active
 }
 
 variables
