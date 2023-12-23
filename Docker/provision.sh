4c4212968e2435d97b7b5a3d4ee0797a280a52b6
lozaexequiel <lozaexequiel@outlook.com>
2023-12-23 16:46:26 -0300

    all packages in one line. sudo to keyrings

--- Docker/provision.sh
+++ Docker/provision.sh
@@ -20,15 +20,14 @@ docker_provision ()
 # Add Docker's official GPG key:
 apt update
 apt install ca-certificates curl gnupg -y
-install -m 0755 -d /etc/apt/keyrings
+sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
-apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
 usermod -G docker ${USER}
 systemctl enable docker
-apt install -y docker-compose
 systemctl restart docker
 systemctl status docker | grep Active
 }
