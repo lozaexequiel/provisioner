b1d499f0679d1a64f079fee1ae453365e8c3b84f
lozaexequiel <lozaexequiel@outlook.com>
2023-12-26 16:49:51 -0300

    new relic version output

--- NewRelic/provision.sh
+++ NewRelic/provision.sh
@@ -23,6 +23,7 @@ ls -la /vagrant_data/.env/
 install_new_relic ()
 {
 curl -Ls ${NEW_RELIC_URL} | bash && sudo NEW_RELIC_API_KEY=${NEW_RELIC_API_KEY} NEW_RELIC_ACCOUNT_ID=${NEW_RELIC_ACCOUNT_ID} ${NEW_RELIC_PATH} install -y --tag ${NEW_RELIC_TAG}
+newrelic --version
 }
 
 header
