571b507ccae1c0f4d79677357623eeedaefcdfbb
lozaexequiel <lozaexequiel@outlook.com>
2024-01-30 15:17:55 -0300

    adds nginx deployment test

---
+++ Nginx/provision.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#set -x
+
+header ()
+{
+echo "#################################################"
+echo "#                                               #"
+echo "#             NGINX PROVISIONER                 #"
+echo "#                                               #"
+echo "#################################################"
+echo " "
+}
+
+variables ()
+{
+if [ ! -f /vagrant_data/.env/.env ]; then
+cp /vagrant_data/.env/.env.example /vagrant_data/.env/.env
+fi
+ls -la /vagrant_data/.env/
+. /vagrant_data/.env/.env
+}
+
+install_nginx ()
+{
+echo "Installing Nginx"
+sudo apt-get update
+docker-compose up -env-file ${ENV_FILE} -d
+docker ps -a
+}
+
+header
+variables
+install_nginx
