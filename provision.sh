e0b45710a1cde32a775b89b779a44baee32877af
lozaexequiel <git@lozaexequiel.com>
2024-03-21 23:10:40 -0300

    generic script

--- provision.sh
+++ provision.sh
@@ -5,7 +5,7 @@ header ()
 echo ""
 echo "#################################################"
 echo "#                                               #"
-echo "#                   PROVISIONER                 #"
+echo "#              ${PROVISIONER_NAME}              #"
 echo "#                                               #"
 echo "#################################################"
 echo ""
@@ -13,6 +13,8 @@ echo ""
 
 variables ()
 {
+hostname=$(hostname)
+ipAddress=$(hostname -I | awk '{print $2}') # Get the second IP address because the first one is the localhost or the private IP address
 if [ ! -d /vagrant_data/.env ]; then
 echo "INFO: The .env directory does not exist or is not detected"
 echo "Creating .env directory in /vagrant_data"
@@ -22,7 +24,7 @@ if [ ! -f /vagrant_data/.env/.env ] || [ ! -s /vagrant_data/.env/.env ]; then
   echo "INFO: The .env file does not exist, is not detected, or the file is empty"
   if [ -f /vagrant_data/.env/.env.example ]; then
     echo "INFO: Example file detected, creating .env file from example"
-    cp /vagrant_data/.env/.env.example /vagrant_data/.env/.env
+    cp /vagrant_data/example/.env.example /vagrant_data/.env/.env
   else
     echo "INFO: Creating an empty .env file"
     touch /vagrant_data/.env/.env
@@ -52,6 +54,14 @@ clean_up ()
 {
 apt autoremove -y
 apt clean
+echo "INFO: The ${PROVISIONER_NAME} has been successfully provisioned in ${hostname} with IP address ${ipAddress}"
+}
+
+permission_ssh_key ()
+{
+chmod 700 ${SSH_DIR}
+chmod 600 ${SSH_DIR}/authorized_keys
+chown -R ${USER}:${USER} ${SSH_DIR}
 }
 
 header
@@ -59,3 +69,4 @@ variables
 disable_swap
 install_dependencies
 clean_up
+permission_ssh_key
