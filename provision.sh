313c551ecb2207c93879b81c17234ea99788d868
lozaexequiel <git@lozaexequiel.com>
2024-04-11 23:09:59 -0300

    new variables func test

--- provision.sh
+++ provision.sh
@@ -13,30 +13,55 @@ echo ""
 
 variables ()
 {
-set -a
-hostname=$(hostname)
-ipAddress=$(hostname -I | awk '{print $2}') # Get the second IP address because the first one is the localhost or the private IP address
-if [ ! -d /vagrant_data/.env ]; then
-echo "INFO: The .env directory does not exist or is not detected"
-echo "Creating .env directory in /vagrant_data"
-mkdir -p /vagrant_data/.env
-fi
-if [ -f /vagrant_data/.env/.env ] && [ -s /vagrant_data/.env/.env ]; then
-echo "Sourcing environment variables from file"
-. /vagrant_data/.env/.env
-else
-echo "INFO: The .env file does not exist, is not detected, or the file is empty"
-if [ -f /vagrant_data/example/.env.example ]; then
-echo "INFO: Example file detected, creating .env file from example"
-cp /vagrant_data/example/.env.example /vagrant_data/.env/.env
-else
-echo "WARNING: The .env file does not exist, is not detected, or the file is empty"
-echo "INFO: Looking for the example file in the main repository"
-curl -s ${EXAMPLE_REMOTE_FILE} -o ${EXAMPLE_LOCAL_FILE}
-fi
-fi
-. /vagrant_data/.env/.env
-set +a
+	set -a
+	hostname=$(hostname)
+	ipAddress=$(hostname -I | awk '{print $2}') # Get the second IP address because the first one is the localhost or the private IP address
+
+	# Define the directory and file paths
+	env_dir="/vagrant_data/.env"
+	env_file="${env_dir}/.env"
+	example_file="/vagrant_data/example/.env.example"
+
+	# Define the remote example file
+	EXAMPLE_REMOTE_FILE=${EXAMPLE_REMOTE_FILE}
+
+	# Check if .env directory exists
+	if [ ! -d "${env_dir}" ]; then
+		echo "INFO: The .env directory does not exist or is not detected"
+		echo "Creating .env directory in ${env_dir}"
+		mkdir -p "${env_dir}"
+	fi
+
+	# Check if .env file exists and is not empty
+	if [ -f "${env_file}" ] && [ -s "${env_file}" ]; then
+		echo "Sourcing environment variables from file"
+		source "${env_file}"
+	else
+		echo "INFO: The .env file does not exist, is not detected, or the file is empty"
+
+		# Check if example file exists
+		if [ -f "${example_file}" ]; then
+			echo "INFO: Example file detected, creating .env file from example"
+			cp "${example_file}" "${env_file}"
+		else
+			echo "INFO: Example file not detected. Downloading from remote location"
+			curl -s "${EXAMPLE_REMOTE_FILE}" -o "${env_file}"
+			if [ $? -eq 0 ]; then
+				echo "INFO: Successfully downloaded the example file"								
+			else
+				echo "ERROR: ${EXAMPLE_REMOTE_FILE} does not exist or failed to download"
+				echo "ERROR: Failed to download the example file. Please check the URL or your network connection"				
+				exit 1
+			fi
+		fi
+
+		if [ ! -s "${env_file}" ]; then
+			echo "ERROR: The .env file is empty, please fill in the required variables"
+			exit 1
+		fi
+	fi
+
+	set +a
 }
 
 disable_swap ()
