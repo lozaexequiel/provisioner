9194f9fd7103029988847113ee3d826aa2d7251c
lozaexequiel <git@lozaexequiel.com>
2024-04-15 10:15:06 -0300

    removes the outputfile / new comments

--- provision.sh
+++ provision.sh
@@ -11,16 +11,15 @@ echo "#################################################"
 echo ""
 }
 
+# Function to set up environment variables
 variables ()
 {
-	set -a
-	hostname=$(hostname)
+	set -a	# Enable automatic export of variables
 	ipAddress=$(hostname -I | awk '{print $2}') # Get the second IP address because the first one is the localhost or the private IP address
-
-	# Define the directory and file paths
+	hostname=$(hostname)
 	env_dir="/vagrant_data/.env"
 	env_file="${env_dir}/.env"
-	example_file="/vagrant_data/example/.env.example"	
+	example_file="/vagrant_data/example/.env.example"
 	EXAMPLE_REMOTE_FILE="https://raw.githubusercontent.com/lozaexequiel/provisioner/main/Ansible/example/.env.example"
 
 	# Check if .env directory exists
@@ -42,24 +41,25 @@ variables ()
 			echo "INFO: Example file detected, creating .env file from example"
 			cp "${example_file}" "${env_file}"
 		else
-			echo "INFO: Example file not detected. Downloading from remote location"
+			echo "WARNING: Example file not detected. Downloading from remote location"
 			curl -L ${EXAMPLE_REMOTE_FILE} -o ${env_file}
 			if [ -s "${env_file}" ]; then
 				echo "INFO: Example file downloaded successfully"
-				source "${env_file}"			
+				source "${env_file}"      
 			else
 				echo "ERROR: ${EXAMPLE_REMOTE_FILE} does not exist or failed to download"
-				echo "ERROR: Failed to download the example file. Please check the URL or your network connection"				
+				echo "ERROR: Failed to download the example file. Please check the URL or your network connection"       
 				exit 1
 			fi
 		fi
 
 		if [ ! -s "${env_file}" ]; then
-			echo "ERROR: The .env file is empty, please fill in the required variables"
+			echo "ERROR: The .env file is empty, please fill in the required variables or download the example file"
 			exit 1
 		fi
 	fi
 
+	# Disable automatic export of variables
 	set +a
 }
 
@@ -80,8 +80,18 @@ clean_up ()
 {
 apt-get autoremove -y
 apt-get clean
-echo "INFO: The ${TOOL} has been successfully provisioned in ${hostname} with IP address ${ipAddress}" >> ${OUTPUT_FILE}
 rm -rf /vagrant_data/functions.sh
+}
+
+# Create a new SSH key in the local path
+create_ssh_key ()
+{
+  mkdir -p ${SSH_DIR}      
+  cd ${SSH_DIR}
+  ssh-keygen -t rsa -b 4096 -C "${USER}@${hostname}" -f ${PRIVATE_KEY_FILE} -N ""
+  chown -R ${USER}:${USER} ${SSH_DIR}
+  eval "$(ssh-agent -s)"
+  ssh-add ${PRIVATE_KEY_FILE}
 }
 
 permission_ssh_key ()
