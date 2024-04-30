a940ee50bce03c4e12ac037032b0c099da3fa29d
lozaexequiel <git@lozaexequiel.com>
2024-04-29 21:10:41 -0300

    ansible function updates

--- Ansible/provision.sh
+++ Ansible/provision.sh
@@ -11,95 +11,34 @@ disable_swap
 install_dependencies
 ansible_provision
 ansible_ssh_key
+permission_ssh_key
 ansible_config
 ansible_inventory
-permission_ssh_key
 ansible_create_vars
 ansible_collection_install
 clean_up
 }
 
+# Function to install extra packages
 ansible_provision ()
 {
-if [ ! -d ${ANSIBLE_PATH} ]; then
-mkdir -p ${ANSIBLE_PATH}
-fi
-if [ -z ${ANSIBLE_VERSION+x} ]; then
-echo "INFO: Installing the latest version of ${EXTRA_PACKAGES}"
-apt-get install -y ${EXTRA_PACKAGES}
-else
-echo "INFO: Installing ${EXTRA_PACKAGES} version ${ANSIBLE_VERSION}"
-apt-get install -y ${EXTRA_PACKAGES}=${ANSIBLE_VERSION}
-fi
-${TEST_COMMAND}
-}
-
-# Function to create ansible ssh key
-ansible_ssh_key ()
-{
-  # Check if the ssh directory exists
-  if [ ! -d "$(dirname ${REMOTE_PUBLIC_KEY_FILE})" ]; then
-    mkdir -p "$(dirname ${REMOTE_PUBLIC_KEY_FILE})"
+  if [ ! -d ${ANSIBLE_PATH} ]; then
+    mkdir -p ${ANSIBLE_PATH}
   fi
 
-  # Remote private key file check
-  if [ -f ${REMOTE_PRIVATE_KEY_FILE} ]; then
-    echo "INFO: Ansible provisioned with custom private key"
-    case $(hostname) in
-      *ansible*)
-        # Copy the remote private key file to the local path
-        cp -f ${REMOTE_PRIVATE_KEY_FILE} ${PRIVATE_KEY_FILE}
-        echo "INFO: The private key has been copied to the local path"
-      ;;
-      *)
-        if [ -f ${REMOTE_PUBLIC_KEY_FILE} ]; then
-          # Check if the public key is in the authorized_keys file
-          if ! grep -q "$(cat ${REMOTE_PUBLIC_KEY_FILE})" ${SSH_DIR}/authorized_keys; then
-            # Add the public key to the authorized_keys file
-            cat ${REMOTE_PUBLIC_KEY_FILE} >> ${SSH_DIR}/authorized_keys
-            echo "INFO: The public key has been added to the authorized_keys file"
-          fi
-        else
-          # Create the public key file
-          ssh-keygen -y -f ${REMOTE_PRIVATE_KEY_FILE} > ${REMOTE_PUBLIC_KEY_FILE}
-          echo "INFO: The public key has been created in the remote path"
-          # Add the public key to the authorized_keys file
-          cat ${REMOTE_PUBLIC_KEY_FILE} >> ${SSH_DIR}/authorized_keys
-          echo "INFO: The public key has been added to the authorized_keys file"
-        fi
-      ;;
-    esac
+  # Update package lists
+  echo "$(date) INFO: Updating package lists"
+  apt-get update || { echo "$(date) ERROR: Failed to update package lists"; exit 1; }
+
+  if [ -z ${ANSIBLE_VERSION+x} ]; then
+    echo "$(date) INFO: Installing the latest version of ${EXTRA_PACKAGES}"
+    apt-get install -y ${EXTRA_PACKAGES} || { echo "$(date) ERROR: Failed to install ${EXTRA_PACKAGES}"; exit 1; }
   else
-    case $(hostname) in
-      *ansible*)
-        # Check if the local private key file already exists
-        if [ ! -f ${PRIVATE_KEY_FILE} ]; then
-          create_ssh_key
-        else
-          echo "INFO: The private key already exists in the local path"
-        fi
-        # Copy the private key file to the remote path
-        cp -f ${PRIVATE_KEY_FILE} ${REMOTE_PRIVATE_KEY_FILE}
-        echo "INFO: The private key has been copied to the remote path"
-        # Check if the remote public key file already exists
-        if [ ! -f ${REMOTE_PUBLIC_KEY_FILE} ]; then
-          # Create the public key file
-          ssh-keygen -y -f ${REMOTE_PRIVATE_KEY_FILE} > ${REMOTE_PUBLIC_KEY_FILE}
-          echo "INFO: The public key has been created in the remote path"
-        fi
-        # Check if the public key is in the authorized_keys file
-        if ! grep -q "$(cat ${REMOTE_PUBLIC_KEY_FILE})" ${SSH_DIR}/authorized_keys; then
-          # Add the public key to the authorized_keys file
-          cat ${REMOTE_PUBLIC_KEY_FILE} >> ${SSH_DIR}/authorized_keys
-          echo "INFO: The public key has been added to the authorized_keys file"
-        fi
-      ;;
-      *)
-        # no actions for this host
-        echo "INFO: No actions for this host"
-      ;;
-    esac
+    echo "$(date) INFO: Installing ${EXTRA_PACKAGES} version ${ANSIBLE_VERSION}"
+    apt-get install -y ${EXTRA_PACKAGES}=${ANSIBLE_VERSION} || { echo "$(date) ERROR: Failed to install ${EXTRA_PACKAGES} version ${ANSIBLE_VERSION}"; exit 1; }
   fi
+
+  ${TEST_COMMAND} || { echo "$(date) ERROR: Test command failed"; exit 1; }
 }
 
 # Function to configure Ansible
@@ -108,92 +47,195 @@ ansible_config ()
   # Check if the hostname includes "ansible"
   case $(hostname) in
     *ansible*)
+      # Check if the Ansible directory exists
+      if [ ! -d ${ANSIBLE_DIR} ]; then
+        echo "$(date) INFO: Creating Ansible directory"
+        mkdir -p ${ANSIBLE_DIR} || { echo "$(date) ERROR: Failed to create ${ANSIBLE_DIR}"; exit 1; }
+      fi
+
       # Check if the Ansible configuration file already exists
       if [ -f ${ANSIBLE_CONFIG} ]; then
         # Copy the existing configuration file
-        cp ${ANSIBLE_CONFIG} ${ANSIBLE_DIR}/ansible.cfg
-        echo "INFO: The ansible.cfg file already exists, you can find the file in ${ANSIBLE_PATH}"
+        cp ${ANSIBLE_CONFIG} ${ANSIBLE_DIR}/ansible.cfg || { echo "$(date) ERROR: Failed to copy ${ANSIBLE_CONFIG}"; exit 1; }
+        echo "$(date) INFO: The ansible.cfg file already exists, you can find the file in ${ANSIBLE_PATH}"
       else
         # Create a new configuration file
-        echo "INFO: The ansible.cfg file does not exist, creating a new one"
-        echo "[defaults]" > ${ANSIBLE_DIR}/ansible.cfg
-        echo "inventory = ${INVENTORY_FILE}" >> ${ANSIBLE_DIR}/ansible.cfg
-        echo "remote_user = ${USER}" >> ${ANSIBLE_DIR}/ansible.cfg
-        echo "private_key_file = ${PRIVATE_KEY_FILE}" >> ${ANSIBLE_DIR}/ansible.cfg
-        echo "remote_tmp = ${REMOTE_TMP}" >> ${ANSIBLE_DIR}/ansible.cfg
-        echo "become_user = ${BECOME_USER}" >> ${ANSIBLE_DIR}/ansible.cfg
-        echo "roles_path = ${ROLES_PATH}" >> ${ANSIBLE_DIR}/ansible.cfg
+        echo "$(date) INFO: The ansible.cfg file does not exist, creating a new one"
+        {
+          echo "[defaults]"
+          echo "inventory = ${INVENTORY_FILE}"
+          echo "remote_user = ${USER}"
+          echo "private_key_file = ${PRIVATE_KEY_FILE}"
+          echo "remote_tmp = ${REMOTE_TMP}"
+          echo "become_user = ${BECOME_USER}"
+          echo "roles_path = ${ROLES_PATH}"
+        } > ${ANSIBLE_DIR}/ansible.cfg || { echo "$(date) ERROR: Failed to create ${ANSIBLE_DIR}/ansible.cfg"; exit 1; }
         # Copy the new configuration file to the Ansible path
-        cp ${ANSIBLE_DIR}/ansible.cfg ${ANSIBLE_PATH}
-        echo "INFO: ansible.cfg file created, you can find the file in ${ANSIBLE_PATH}"
+        cp ${ANSIBLE_DIR}/ansible.cfg ${ANSIBLE_PATH} || { echo "$(date) ERROR: Failed to copy ${ANSIBLE_DIR}/ansible.cfg"; exit 1; }
+        echo "$(date) INFO: ansible.cfg file created, you can find the file in ${ANSIBLE_PATH}"
       fi
       ;;
   esac
 }
 
+# Function to create Ansible inventory
 ansible_inventory ()
 {
     case $(hostname) in
     *ansible*)
+    # Check if the Ansible directory exists
+    if [ ! -d ${ANSIBLE_PATH} ]; then
+      echo "$(date) INFO: Creating Ansible directory"
+      mkdir -p ${ANSIBLE_PATH} || { echo "$(date) ERROR: Failed to create ${ANSIBLE_PATH}"; exit 1; }
+    fi
+
     if [ -f ${INVENTORY_FILE} ]; then
-      echo "INFO: The INVENTORY_FILE file already exists"
-else
-      echo "INFO: The INVENTORY_FILE file does not exist, creating a new one"
-      echo "[master]" > ${INVENTORY_FILE}
-      echo "" >> ${INVENTORY_FILE}
-      echo "[node]" >> ${INVENTORY_FILE}
-      echo "" >> ${INVENTORY_FILE}
-      echo "[other]" >> ${INVENTORY_FILE}
-      echo "" >> ${INVENTORY_FILE}
-      echo "INFO: INVENTORY_FILE file created, you can find the file in ${ANSIBLE_PATH}"
+      echo "$(date) INFO: The INVENTORY_FILE file already exists"
+    else
+      echo "$(date) INFO: The INVENTORY_FILE file does not exist, creating a new one"
+      {
+        echo "[master]"
+        echo ""
+        echo "[node]"
+        echo ""
+        echo "[other]"
+      } > ${INVENTORY_FILE} || {
+        echo "$(date) ERROR: Failed to create INVENTORY_FILE"
+        exit 1
+      }
+      echo "$(date) INFO: INVENTORY_FILE file created, you can find the file in ${ANSIBLE_PATH}"
     fi
     ;;
-    *master*)
-      awk -v hostline="${hostname} ansible_host=${ipAddress} ansible_user=${USER} ansible_ssh_private_key_file=${PRIVATE_KEY_FILE}" '/\[master\]/ { print; print hostline; next }1' "${INVENTORY_FILE}" > temp && mv temp "${INVENTORY_FILE}"
-      echo "INFO: ${hostname} host added to the [master] group"
-      ;;
-    *node*)
-      awk -v hostline="${hostname} ansible_host=${ipAddress} ansible_user=${USER} ansible_ssh_private_key_file=${PRIVATE_KEY_FILE}" '/\[node\]/ { print; print hostline; next }1' "${INVENTORY_FILE}" > temp && mv temp "${INVENTORY_FILE}"
-      echo "INFO: ${hostname} host added to the [node] group"
-      ;;
-    *)
-      echo "WARNING: The hostname must contain 'master' or 'node' to run this script, next steps could be affected"
-      echo "WARNING: ${hostname} host added to the [other] group"
-      awk -v hostline="${hostname} ansible_host=${ipAddress} ansible_user=${USER} ansible_ssh_private_key_file=${PRIVATE_KEY_FILE}" '/\[other\]/ { print; print hostline; next }1' "${INVENTORY_FILE}" > temp && mv temp "${INVENTORY_FILE}"
+    *master*|*node*|*)
+      group="other"
+      [ "$(hostname)" = "*master*" ] && group="master"
+      [ "$(hostname)" = "*node*" ] && group="node"
+      temp_file=$(mktemp)
+      awk -v hostline="${hostname} ansible_host=${ipAddress} ansible_user=${USER} ansible_ssh_private_key_file=${PRIVATE_KEY_FILE}" '/\['"$group"'\]/ { print; print hostline; next }1' "${INVENTORY_FILE}" > "$temp_file" && mv "$temp_file" "${INVENTORY_FILE}" || {
+        echo "$(date) ERROR: Failed to update INVENTORY_FILE"
+        exit 1
+      }
+      echo "$(date) INFO: ${hostname} host added to the [$group] group"
       ;;
   esac
 }
 
+# Function to create Ansible variables
 ansible_create_vars ()
 { 
   case $(hostname) in
     *ansible*)
     if [ -f ${PLAYBOOK_VARS} ]; then
-      echo "INFO: The PLAYBOOK_VARS file already exists"
+      echo "$(date) INFO: The PLAYBOOK_VARS file already exists"
     else
-      echo "INFO: The PLAYBOOK_VARS file does not exist, trying to download from remote location"
-      curl -L ${EXAMPLE_TOOL_FILE} -o ${PLAYBOOK_VARS}
-      if [ -f ${PLAYBOOK_VARS} ]; then
-        echo "INFO: PLAYBOOK_VARS file downloaded successfully"
-      else
-        echo "ERROR: ${EXAMPLE_TOOL_FILE} does not exist or failed to download"
-        echo "ERROR: Failed to download the example file. Please check the URL or your network connection"
+      echo "$(date) INFO: The PLAYBOOK_VARS file does not exist, trying to download from remote location"
+      # Check if curl is available
+      if ! command -v curl &> /dev/null; then
+        echo "$(date) ERROR: curl could not be found. Please install it and try again"
         exit 1
-    fi
+      fi
+      if curl -L ${EXAMPLE_TOOL_FILE} -o ${PLAYBOOK_VARS}; then
+        echo "$(date) INFO: PLAYBOOK_VARS file downloaded successfully"
+      else
+        echo "$(date) ERROR: Failed to download the example file. Please check the URL or your network connection"
+        exit 1
+      fi
     fi
     ;;
     *)
-    echo "INFO: no actions for this host"
+    echo "$(date) INFO: no actions for this host"
     ;;
   esac
 }
 
+# Function to install Ansible collections
 ansible_collection_install ()
 {
   if [ -n "${COLLECTIONS}" ]; then
-    echo "INFO: Installing Ansible collections"
-    ansible-galaxy collection install ${COLLECTIONS}
+    echo "$(date) INFO: Installing Ansible collections"
+    # Check if ansible-galaxy is available
+    if ! command -v ansible-galaxy &> /dev/null; then
+      echo "$(date) ERROR: ansible-galaxy could not be found. Please install it and try again"
+      exit 1
+    fi
+    if ansible-galaxy collection install ${COLLECTIONS}; then
+      echo "$(date) INFO: Ansible collections installed successfully"
+    else
+      echo "$(date) ERROR: Failed to install Ansible collections"
+      exit 1
+    fi
+  else
+    echo "$(date) INFO: No collections detected"
   fi
+}
+
+# Function to create ansible ssh key
+ansible_ssh_key ()
+{
+  case $(hostname) in *ansible*)
+  # Check if the SSH directory exists, if not create it
+  mkdir -p ${SSH_DIR}
+  private_key
+  public_key
+  ;;
+  *)
+  public_key
+  ;;
+  esac
+  permission_ssh_key
+}
+
+private_key ()
+
+{
+    # Check if the remote private key file exists
+  if [ -f ${REMOTE_PRIVATE_KEY_FILE} ]; then
+    # Copy the remote private key file to the local path
+    cp -f ${REMOTE_PRIVATE_KEY_FILE} ${PRIVATE_KEY_FILE}
+    echo "$(date) INFO: The private key has been copied to the local path"
+  else
+    # No remote private key, check if local private key exists
+    if [ ! -f ${PRIVATE_KEY_FILE} ]; then
+      # No local private key, call create_ssh_key to generate a new key pair
+      create_ssh_key
+      echo "$(date) INFO: A new SSH key pair has been created"
+      # Check if the PRIVATE_KEY_FILE exists in the local path after it's created
+      if [ -f ${PRIVATE_KEY_FILE} ]; then
+        # Copy the PRIVATE_KEY_FILE to the REMOTE_PRIVATE_KEY_FILE
+        cp -f ${PRIVATE_KEY_FILE} ${REMOTE_PRIVATE_KEY_FILE}
+        echo "$(date) INFO: The private key has been copied to the remote path"
+      fi
+    fi
+  fi
+}
+
+public_key ()
+{
+  if [ -f ${REMOTE_PUBLIC_KEY_FILE} ]; then
+    if ! grep -q "$(cat ${REMOTE_PUBLIC_KEY_FILE})" ${SSH_DIR}/authorized_keys; then
+      cat ${REMOTE_PUBLIC_KEY_FILE} >> ${SSH_DIR}/authorized_keys || { echo "$(date) ERROR: Failed to add public key to authorized_keys"; exit 1; }
+      echo "$(date) INFO: The public key has been added to the authorized_keys file"
+    else
+      echo "$(date) INFO: The public key is already in the authorized_keys file"
+    fi
+  else
+    ssh-keygen -y -f ${PRIVATE_KEY_FILE} > ${REMOTE_PUBLIC_KEY_FILE} || { echo "$(date) ERROR: Failed to create public key"; exit 1; }
+    echo "$(date) INFO: The public key has been created in the remote path"
+    cat ${REMOTE_PUBLIC_KEY_FILE} >> ${SSH_DIR}/authorized_keys || { echo "$(date) ERROR: Failed to add public key to authorized_keys"; exit 1; }
+    if ! grep -q "$(cat ${REMOTE_PUBLIC_KEY_FILE})" ${SSH_DIR}/authorized_keys; then
+      echo "$(date) ERROR: The public key is not in the authorized_keys file"
+      exit 1
+    else
+      echo "$(date) INFO: The public key has been added to the authorized_keys file"
+    fi
+  fi
+}
+
+permission_ssh_key ()
+{
+  chmod 700 ${SSH_DIR} || { echo "$(date) ERROR: Failed to change permissions for SSH_DIR"; exit 1; }
+  chmod 600 ${HOME}/.ssh/authorized_keys || { echo "$(date) ERROR: Failed to change permissions for authorized_keys"; exit 1; }
+  chown -R ${USER}:${USER} ${SSH_DIR} || { echo "$(date) ERROR: Failed to change owner for SSH_DIR"; exit 1; }
 }
 
 provision
